d8364cbfca652f840607cffefa4811c8ed3a9349
Moncef HILALI <hilali.moncef@gmail.com>
2019-12-13 00:11:10 +0100

    - Adding migrations and final support for Heroku

--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '-#-_u89qpd589wtt3-0*_xp90t9-u)&0%6w7_hmjfix4v5k%ly'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
